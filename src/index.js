82648da496d4f068b747f410cf43ba8ea8f181ab
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-09-01 15:05:37 +0530

    created raw http server, added socket.io module, bound httpserver to socketio server, served up client side socketio.js, added new web socket connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const path = require('path')
+require('dotenv').config()
+const path = require('path') // core module
 const express = require('express')
 const app=express();
-const port = process.env.PORT || 5000
-//app.set('view engine','ejs')
+const http = require('http') //core module
+const server = http.createServer(app)
+const socketio = require('socket.io')
+const io = socketio(server)
+const port = process.env.PORT 
+
 app.use(express.static(path.join(__dirname,'../public')))
 
-app.listen(port,()=>{
+io.on('connection',()=>{
+    console.log("Web Socket Connection on!");
+})
+
+server.listen(port,()=>{
     console.log("App listening to port",port);
 })
