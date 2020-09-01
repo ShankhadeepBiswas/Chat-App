900136752a5357ca3cef7b39d290d49b04c03726
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-09-01 14:10:27 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const path = require('path')
 const express = require('express')
 const app=express();
 const port = process.env.PORT || 5000
-app.set('view engine','ejs')
+//app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'../public')))
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+
 app.listen(port,()=>{
     console.log("App listening to port",port);
 })
