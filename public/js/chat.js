2bcc186eb1e82f49993b4b78ecde48b769618551
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-09-03 22:21:30 +0530

    Event ackncowledgements mainly,adding callbacks, sending google map locations of users

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,12 @@ socket.on('Welcome',(message)=>{                  // receives from server side
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
   e.preventDefault()
   let msg = e.target.elements.message.value
-  socket.emit('Message',msg)
+  socket.emit('Message',msg,(error)=>{                 //callbacks are event aknowledgements, assuring client received from server.
+    if(error){
+      return console.log(error);
+    }
+    console.log("Message has been delivered");
+  })
 })
 document.querySelector('#Location-btn').addEventListener('click',()=>{
   if(!navigator.geolocation){
@@ -13,6 +18,8 @@ document.querySelector('#Location-btn').addEventListener('click',()=>{
   }
   navigator.geolocation.getCurrentPosition((position)=>{
     const {latitude: lat,longitude: lon} = position.coords
-    socket.emit('location',{lat,lon})
+    socket.emit('location',{lat,lon},()=>{
+      console.log("Location has been sent!");
+    })
   })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const path = require('path') // core module
 const express = require('express')
 const app=express();
+const Filter = require('bad-words')
 const http = require('http') //core module
 const server = http.createServer(app)
 const socketio = require('socket.io')
@@ -11,13 +12,19 @@ const port = process.env.PORT
 app.use(express.static(path.join(__dirname,'../public')))
 io.on('connection',(socket)=>{             //socket contains info about connected client
     console.log("Web Socket Connection on!");
+    const filter = new Filter()
     socket.emit('Welcome','Welcome!')            // sending to client side
     socket.broadcast.emit('Welcome','A new user has joined!')
-    socket.on('Message',(msg)=>{
+    socket.on('Message',(msg,callback)=>{
+        if(filter.isProfane(msg)){
+            return callback('Profanity is not allowed!');
+        }
         io.emit('Welcome',msg)        //io sends to all connected clients at once. socket sends to individual.
+        callback()
     })
-    socket.on('location',(obj)=>{
-        io.emit('Welcome',`Location: Latitude:${obj.lat} and Longitude:${obj.lon}`)
+    socket.on('location',(obj,callback)=>{
+        io.emit('Welcome',`https://www.google.com/maps?q=${obj.lat},${obj.lon}`)
+        callback()
     })
     socket.on('disconnect',()=>{
         io.emit('Welcome','The user has left!')
